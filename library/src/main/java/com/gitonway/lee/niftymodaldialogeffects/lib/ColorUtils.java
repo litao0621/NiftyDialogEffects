5e1678b6804b5f42317dac4c5a334ddc71d523ff
Scott A Garman <sag5066@gmail.com>
2014-10-06 13:31:14 -0700

    Adding support for custom dialog colors.

---
+++ library/src/main/java/com/gitonway/lee/niftymodaldialogeffects/lib/ColorUtils.java
@@ -0,0 +1,24 @@
+package com.gitonway.lee.niftymodaldialogeffects.lib;
+
+import android.graphics.ColorFilter;
+import android.graphics.ColorMatrixColorFilter;
+
+
+public class ColorUtils {
+    // http://stackoverflow.com/a/11171509/317862
+    public static ColorFilter getColorFilter(int color) {
+        ColorMatrixColorFilter colorFilter;
+        int red = (color & 0xFF0000) / 0xFFFF;
+        int green = (color & 0xFF00) / 0xFF;
+        int blue = color & 0xFF;
+
+        float[] matrix = { 0, 0, 0, 0, red
+                , 0, 0, 0, 0, green
+                , 0, 0, 0, 0, blue
+                , 0, 0, 0, 1, 0 };
+
+        colorFilter = new ColorMatrixColorFilter(matrix);
+
+        return colorFilter;
+    }
+}
